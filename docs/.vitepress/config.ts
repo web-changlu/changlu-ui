30000653310c1675e8e6cc479936452680e1e9bc
wb_hujiangtao <wb_hujiangtao@meituan.com>
2024-01-19 11:48:02 +0800

    githubpages

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,145 @@
+import { defineConfig } from 'vitepress'
+const getDemoSidebar = () => {
+  return [
+    {
+      text: "介绍",
+      link: '/components/introduce/',
+      children: []
+    },
+    {
+      text: "更新日志",
+      children: [
+        {
+          text: "新特性",
+          link: "/components/log/new",
+          children: [],
+        },
+        {
+          text: '往期记录',
+          link: '/components/log/index',
+          children: []
+        }
+      ]
+    },
+    {
+      text: "开发指南",
+      children: [
+        {
+          text: "安装",
+          link: '/components/install/index',
+          children: []
+        },
+        {
+          text: "快速开始",
+          link: "/components/install/quick",
+          children: []
+        },
+      ]
+    },
+
+    {
+      text: "基础组件",
+      children: [
+
+        {
+          text: "Button 按钮",
+          link: "/components/button/",
+          children: []
+        },
+
+        
+        {
+          text: "Icon 图标",
+          link: "/components/icon/",
+          children: []
+        },
+      ],
+    },
+    {
+      text: "导航组件",
+      children: [
+        {
+          text: "NavigationBar 导航栏",
+          link: "/components/navigationbar/",
+          children: []
+        },
+      ]
+    },
+    {
+      text: "表单组件",
+      children: [
+        {
+          text: "Selector 选择器",
+          link: "/components/selector/",
+          children: []
+        },
+      ]
+    },
+    {
+      text: "创意组件",
+      link: "/components/creative/",
+      children: [
+        {
+          text: "Clock 时钟",
+          link: "/components/clock/",
+          children: []
+        },
+        {
+          text: "WordCloud 词云",
+          link: "/components/wordCloud/",
+          children: []
+        },
+      ]
+    },
+    {
+      text: "布局组件",
+      children: []
+    },
+  ]
+}
+export default defineConfig({
+  base: '/changlu-ui/',
+  title: 'ChangLu-UI',
+  description: '一款v3组件库',
+  head: [[
+    'link', {
+      rel: 'icon',
+      href: '/changlu-ui/img/favicon.ico'
+    }
+  ]],
+  themeConfig: {
+    //   展示搜索框
+    algolia: {
+      apiKey: '',
+      indexName: '',
+      searchParameters: {
+        faeFilters: ['tags:guide,api']
+      }
+    },
+    nav: [{
+      text: 'Home',
+      link: '/',
+    },
+    {
+      text: 'GitHub',
+      link: 'https://github.com/web-changlu',
+    },
+    {
+      text: 'Npm',
+      link: 'https://www.npmjs.com/package/changlu-ui',
+    },
+    ],
+    // 侧边栏
+    sidebar: {
+      '/': getDemoSidebar(),
+    }
+  },
+  markdown: {
+    config: (md) => {
+      const {
+        demoBlockPlugin
+      } = require('vitepress-theme-demoblock')
+      md.use(demoBlockPlugin)
+    }
+  }
+})
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,20 @@
+
+import '../../../dist/theme-default/index.css'
+// 导入vitepress-theme-demoblock主题，并注册组件(包含主题中默认的组件)。
+import Theme from 'vitepress/dist/client/theme-default'
+
+import type { App } from 'vue'
+// 导入主题样式
+import 'vitepress-theme-demoblock/theme/styles/index.css'
+// import clButton from '../../../components/lib/Button/src/main.vue'
+// 导入插件的主题
+import { registerComponents } from './register-components.js'
+
+export default {
+  base: '/changlu-ui/',
+  ...Theme,
+  enhanceApp(vitepress:any) {
+    // app.component('clButton', clButton) 
+    registerComponents(vitepress.app)
+  }
+}
